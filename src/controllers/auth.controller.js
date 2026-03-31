4fe0215eac6fb49337209408211c9d51c6775082
rezafauzan <rezafauzan593@gmail.com>
2026-03-31 17:59:07 +0700

    Feat: add auth controller and login method

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,69 @@
+import * as userModel from "../models/users.model.js"
+
+/**
+ * 
+ * @param {import("express").Request} request 
+ * @param {import("express").Response} response 
+ */
+export async function login(request, response) {
+    const { email, password } = request.body
+    if (email !== "" && email.includes("@")) {
+        try {
+            const [user, index] = await userModel.getUserByEmail(email)
+            if (!user) {
+                response.json(
+                    {
+                        success: false,
+                        messages: "Login fail! wrong email or password",
+                        result: null
+                    }
+                )
+                return
+            }
+            
+            if (user.password !== password){
+                response.json(
+                    {
+                        success: false,
+                        messages: "Login fail! wrong email or password",
+                        result: null
+                    }
+                )
+                return
+            }
+
+            response.json(
+                {
+                    success: true,
+                    messages: "Login success!",
+                    result: user
+                }
+            )
+        } catch (error) {
+            response.json(
+                {
+                    success: false,
+                    messages: "Login failed! " + error,
+                    result: null
+                }
+            )
+        }
+    } else {
+        response.json(
+            {
+                success: false,
+                messages: "Login failed! email invalid",
+                result: null
+            }
+        )
+    }
+}
+
+/**
+ * 
+ * @param {import("express").Request} request 
+ * @param {import("express").Response} response 
+ */
+export async function register(request, response) {
+    const { email, password } = request.body
+}
