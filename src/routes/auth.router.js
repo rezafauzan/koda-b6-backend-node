42b2db3b0a3d99d3babae307f6f22d2d89db69be
rezafauzan <rezafauzan593@gmail.com>
2026-03-31 18:27:51 +0700

    Fix: wrong handler on post /auth/register

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -3,6 +3,6 @@ import * as authController from "../controllers/auth.controller.js"
 const authRouter = Router()
 
 authRouter.post("/login", authController.login)
-authRouter.post("/register", authController.login)
+authRouter.post("/register", authController.register)
 
 export default authRouter
