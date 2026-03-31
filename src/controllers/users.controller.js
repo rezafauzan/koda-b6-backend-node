9f79eebf96a1bd209b6223250f2cba525ce27e69
rezafauzan <rezafauzan593@gmail.com>
2026-03-31 11:56:32 +0700

    Feat: add user create validation

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -21,6 +21,23 @@ export async function getAllUsers(request, respond){
  */
 export async function createUsers(request, respond){
     const data = request.body
+    if(!data.email.includes("@")){
+        respond.json({
+            success: false,
+            message: "Create user failed : Invalid email",
+            result: null
+        })
+        return
+    }
+    if(!data.password.length >= 8){
+        respond.json({
+            success: false,
+            message: "Create user failed : Password too weak! minimum 8 characters",
+            result: null
+        })
+        return
+    }
+
     const users = await userModel.createUsers(data)
     respond.json({
         success: true,
