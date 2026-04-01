43e192abb20a6c92981b5b75d9f4d957dbc4ba44
rezafauzan <rezafauzan593@gmail.com>
2026-04-01 13:20:58 +0700

    Feat: add auth middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,27 @@
+import { verifyToken } from "../lib/jwt.js"
+
+/**
+ * 
+ * @param {import("express").Request} request 
+ * @param {import("express").Response} response 
+ * @param {import("express").NextFunction} next 
+ */
+export default function Auth(request, response, next) {
+    const authHeader = request.headers.authorization
+    const prefix = "Bearer "
+    const isBearer = authHeader?.startsWith(prefix)
+    if (isBearer) {
+        const token = authHeader.slice(prefix.length)
+        const payload = verifyToken(token)
+        if (payload) {
+            response.locals = payload
+            next()
+            return
+        }
+    }
+    response.status(400).json({
+        success: false,
+        messages: "Unauthorized access!",
+        result: null
+    })
+}
