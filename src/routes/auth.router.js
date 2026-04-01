a3803d7a9b6868d9e9c1b116d509434ef53267fd
rezafauzan <rezafauzan593@gmail.com>
2026-04-01 13:21:39 +0700

    Feat: add documentation on /auth/login

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -2,6 +2,28 @@ import { Router } from "express";
 import * as authController from "../controllers/auth.controller.js"
 const authRouter = Router()
 
+/**
+ * @openapi
+ * /auth/login:
+ *  post:
+ *   tags: ['auth']
+ *   description: Log into system with registered user
+ *   requestBody:
+ *       content: 
+ *           application/json: 
+ *              schema:
+ *                  type: object
+ *                  properties:
+ *                      email: 
+ *                          type: string
+ *                      password:
+ *                          type: string
+ *   responses:
+ *       200:
+ *           description: 'login success'
+ *       403:
+ *           description: 'login fail'
+ */
 authRouter.post("/login", authController.login)
 authRouter.post("/register", authController.register)
 
