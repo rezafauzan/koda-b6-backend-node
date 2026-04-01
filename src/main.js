d7e6155e5428311f7d6f9772264cc54f9490b5e9
rezafauzan <rezafauzan593@gmail.com>
2026-04-01 13:18:24 +0700

    Feat: add swagger router and documentation on main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import express from "express"
 import {constants} from "node:http2"
+import docsRouter from "./routes/docs.router.js"
 import authRouter from "./routes/auth.router.js"
 import userRouter from "./routes/users.router.js"
 
 const app = express()
-const port = process.env.PORT || 8888
+const port = 8888
 
 app.use(express.json())
-
+/**
+ * @openapi
+ * /:
+ *  get:
+ *      tags: ['Health Check']
+ *      description: Health Check
+ *      responses:
+ *          200:
+ *              description: Returning JSON with success and message
+ */
 app.get("/", function(request, respond){
     console.log("OK")
     respond.status(constants.HTTP_STATUS_OK).json({
@@ -17,6 +27,7 @@ app.get("/", function(request, respond){
     })
 })
 
+app.use("/docs", docsRouter)
 app.use("/auth", authRouter)
 app.use("/admin/users", userRouter)
 
