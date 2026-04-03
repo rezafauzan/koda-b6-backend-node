fd7ad58997cf0738da15231465a6e12ac5d390f1
rezafauzan <rezafauzan593@gmail.com>
2026-04-03 16:19:27 +0700

    Feat: add user_profiles router

---
+++ src/routes/user_profiles.router.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import AuthMiddleware from "../middleware/auth.middleware.js";
+import * as userProfileController from "../controllers/user_profiles.controller.js"
+
+const userProfileRouter = Router()
+
+// userProfileRouter.use(AuthMiddleware)
+
+userProfileRouter.get("", userProfileController.getUserProfileById)
+
+export default userProfileRouter
