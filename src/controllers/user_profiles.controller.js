d304330a8da8b968dcb32a9c0c1bf4c9bc7e2371
rezafauzan <rezafauzan593@gmail.com>
2026-04-03 16:19:48 +0700

    Feat: add user_profiles controller

---
+++ src/controllers/user_profiles.controller.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @param {import("express").Request} request 
+ * @param {import("express").Response} response 
+ */
+export async function getUserProfileById(request, response) {
+    console.log(request.headers)
+}
