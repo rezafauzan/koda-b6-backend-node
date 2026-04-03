2073a62aff53b1900b61eef018a521ab75aad77c
rezafauzan <rezafauzan593@gmail.com>
2026-04-03 17:17:36 +0700

    Feat: add getUserProfileById user_profile controller

--- src/controllers/user_profiles.controller.js
+++ src/controllers/user_profiles.controller.js
@@ -1,8 +1,39 @@
+import { verifyToken } from "../lib/jwt.js"
+import * as userProfilesModel from "../models/user_profiles.model.js"
+
 /**
  * 
  * @param {import("express").Request} request 
  * @param {import("express").Response} response 
  */
 export async function getUserProfileById(request, response) {
-    console.log(request.headers)
+    const authHeader = request.headers.authorization
+    console.log(authHeader)
+    const prefix = "Bearer "
+    const isBearer = authHeader?.startsWith(prefix)
+    if (isBearer) {
+        const token = authHeader.slice(prefix.length)
+        const payload = verifyToken(token)
+        if (!payload) {
+            response.json({
+                success: true,
+                message: "Failed get current user profile! token invalid",
+                result: null
+            })       
+        }
+        try {
+            const userProfile = await userProfilesModel.getUserProfileById(payload.id)
+            response.json({
+                success: true,
+                message: "Success get current user profile!",
+                result: userProfile
+            })
+        } catch (error) {
+            response.json({
+                success: true,
+                message: "Failed get current user profile! " + error,
+                result: null
+            })       
+        }
+    }
 }
