7dee8dd95267334ff1a231653cb1d2e0cf753c64
rezafauzan <rezafauzan593@gmail.com>
2026-04-03 16:06:18 +0700

    Feat: add user_profiles model

---
+++ src/models/user_profiles.model.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef {Object} UserProfile
+ * @property {number} id
+ * @property {number} user_id
+ * @property {string} user_avatar
+ * @property {string} first_name
+ * @property {string} last_name
+ * @property {string} address
+ * @property {string} created_at
+ * @property {string} updated_at
+ */
