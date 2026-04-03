38d01ed7b4f5ea7a22117e3b10cba4cb228b6d76
rezafauzan <rezafauzan593@gmail.com>
2026-04-03 19:11:09 +0700

    Feat: add getUserCredentialsByEmail user_credentials model

--- src/models/user_credentials.model.js
+++ src/models/user_credentials.model.js
@@ -13,7 +13,7 @@ import db from "../lib/db.js"
 
 /**
  * @param {number} userId
- * @returns {Promise<UserCredentials>}
+ * @returns {UserCredentials}
  */
 export async function getUserCredentialsByUserId(userId) {
     const sql = `SELECT id, user_id, email, phone, password, created_at, updated_at FROM user_credentials WHERE user_id = $1`
@@ -24,6 +24,22 @@ export async function getUserCredentialsByUserId(userId) {
     }
     return result.rows[0]
 }
+
+/**
+ * 
+ * @param {string} email 
+ * @returns {UserCredentials}
+*/
+export async function getUserCredentialsByEmail(email) {
+    const sql = `SELECT user_id, email, phone, password, created_at, updated_at FROM user_credentials WHERE email = $1`
+    const values = [email]
+    const result = await db().query(sql, values)
+    if (result.rows.length < 1) {
+        throw new Error("Users not found !");
+    }
+    return result.rows[0]
+}
+
 
 /**
  * @param {Object} newUserCredentialsData
