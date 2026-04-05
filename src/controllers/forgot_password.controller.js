f9675dccfaec7d738c80366713b011c11eda1da6
rezafauzan <rezafauzan593@gmail.com>
2026-04-06 04:20:23 +0700

    Feat: add resetPassword forgot_password.controller.js

--- src/controllers/forgot_password.controller.js
+++ src/controllers/forgot_password.controller.js
@@ -48,3 +48,62 @@ export async function requestForgotPassword(request, response) {
         });
     }
 }
+
+/**
+ * @param {import("express").Request} request 
+ * @param {import("express").Response} response 
+ */
+export async function resetPassword(request, response) {
+    try {
+        const { email, code_otp, new_password, password_confirm } = request.body;
+        if (!email || !email.includes("@")) {
+            throw new Error("Invalid email format !");
+        }
+
+        if (!new_password || new_password.length < 8) {
+            throw new Error("Password too weak minimum length is 8 characters !");
+        }
+
+        if (new_password !== password_confirm) {
+            throw new Error("Password confirmation mismatch !");
+        }
+
+        const user = await userCredentialsModel.getUserCredentialsByEmail(email);
+
+        if (!user) {
+            throw new Error("User not registered !");
+        }
+
+        const latestOtp = await forgotPasswordModel.GetLatestOTP(email);
+
+        if (!latestOtp) {
+            throw new Error("Invalid OTP !");
+        }
+
+        if (parseInt(latestOtp.code_otp) !== parseInt(code_otp)) {
+            throw new Error("Invalid OTP !");
+        }
+
+        const hashedPassword = await GenerateHash(new_password)
+
+        const updatedUser = await userCredentialsModel.updatePasswordByEmail(email,{hashedPassword})
+        if (!updatedUser) {
+            throw new Error("Failed to update password !");
+        }
+
+        await forgotPasswordModel.deleteOTPByEmail(email);
+
+        response.json({
+            success: true,
+            message: "Reset password success !",
+            result: null
+        });
+
+    } catch (error) {
+        response.json({
+            success: false,
+            message: "Reset password failed ! " + error.message,
+            result: null
+        });
+    }
+}
