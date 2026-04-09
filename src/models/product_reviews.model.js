a6a479e46b5de3bb22ddcc432ea68beb146574cb
rezafauzan <rezafauzan593@gmail.com>
2026-04-09 07:41:05 +0700

    Feat: add product_reviews.model.js

---
+++ src/models/product_reviews.model.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef {Object} ProductReview
+ * @property {number} id
+ * @property {number} product_id
+ * @property {number} user_id
+ * @property {number} rating
+ * @property {string} messages
+ * @property {string} created_at
+ * @property {string} updated_at
+ */
+
