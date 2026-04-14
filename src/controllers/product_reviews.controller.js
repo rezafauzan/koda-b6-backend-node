45324616108f68a8fff5f9cd56538c914a9c0242
rezafauzan <rezafauzan593@gmail.com>
2026-04-14 16:43:25 +0700

    Feat: add getPopularProduct on product_reviews.controller.js

--- src/controllers/product_reviews.controller.js
+++ src/controllers/product_reviews.controller.js
@@ -13,3 +13,17 @@ export async function getAllProductsReviews(request, response) {
         result: productsReviews
     })
 }
+
+/**
+ * 
+ * @param {import("express").Request} request 
+ * @param {import("express").Response} response 
+ */
+export async function getPopularProducts(request, response) {
+    const popularProducts = await productReviewsModel.getPopularProducts()
+    response.json({
+        success: true,
+        message: "Get popular products",
+        result: popularProducts
+    })
+}
