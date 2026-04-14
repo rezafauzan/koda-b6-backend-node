0732e95cd54a8ae69ba436b1f9c7421436d9a0c8
rezafauzan <rezafauzan593@gmail.com>
2026-04-14 16:43:16 +0700

    Feat: add getPopularProduct on product_reviews.model.js

--- src/models/product_reviews.model.js
+++ src/models/product_reviews.model.js
@@ -20,3 +20,14 @@ export async function getAllProductsReviews() {
     const result = await db().query(sql)
     return result.rows[0] ?? null
 }
+
+/**
+ * @return {ProductReview[]}
+ */
+export async function getPopularProducts() {
+    const sql = `SELECT products.id, products.category_id, products.name, products.description, products.price, products.stock, products.created_at, products.updated_at, product_reviews.total_reviews FROM products JOIN (SELECT product_id, COUNT(product_id) AS total_reviews FROM product_reviews GROUP BY product_id ORDER BY total_reviews DESC LIMIT $1) AS product_reviews ON products.id = product_reviews.product_id ORDER BY product_reviews.total_reviews DESC`
+    const limit = 4
+    const values = [limit]
+    const result = await db().query(sql, values)
+    return result.rows[0] ?? null
+}
