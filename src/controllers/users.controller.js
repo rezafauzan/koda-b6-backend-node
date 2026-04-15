4bf629779254f5a76d3e8907405bba60ee436b18
rezafauzan <rezafauzan593@gmail.com>
2026-04-15 17:12:00 +0700

    feat: update src/controllers/users.controller.js using http helper

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,4 +1,5 @@
 import { GenerateHash } from "../lib/hash.js"
+import { httpResponse } from "../lib/http_handlers.js"
 import * as userCredentialsModel from "../models/user_credentials.model.js"
 import * as userModel from "../models/users.model.js"
 
@@ -8,12 +9,20 @@ import * as userModel from "../models/users.model.js"
  * @param {import("express").Response} response 
  */
 export async function getAllUsers(request, response) {
-    const users = await userModel.getAllUsers()
-    response.json({
-        success: true,
-        message: "Get all users data",
-        result: users
-    })
+    try {
+        const users = await userModel.getAllUsers()
+
+        return httpResponse.ok(
+            response,
+            "Get all users data",
+            users
+        )
+    } catch (error) {
+        return httpResponse.serverError(
+            response,
+            "Failed get all users data " + error
+        )
+    }
 }
 
 /**
@@ -22,99 +31,101 @@ export async function getAllUsers(request, response) {
  * @param {import("express").Response} response 
  */
 export async function createUsers(request, response) {
-    const { first_name, last_name, address, phone, email, password, confirm_password } = request.body
+    const {
+        first_name,
+        last_name,
+        address,
+        phone,
+        email,
+        password,
+        confirm_password
+    } = request.body
 
     if (first_name === undefined || first_name.length < 4) {
-        response.json({
-            success: false,
-            message: "Create user failed : First Name minimum 4 characters",
-            result: null
-        })
-        return
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : First Name minimum 4 characters"
+        )
     }
 
     if (last_name === undefined || last_name.length < 4) {
-        response.json({
-            success: false,
-            message: "Create user failed :Last Name minimum 4 characters",
-            result: null
-        })
-        return
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : Last Name minimum 4 characters"
+        )
     }
 
     if (phone === undefined || phone.length < 10) {
-        response.json({
-            success: false,
-            message: "Create user failed : Phone Number minimum 10 digits",
-            result: null
-        })
-        return
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : Phone Number minimum 10 digits"
+        )
     }
 
     if (address === undefined || address.length < 10) {
-        response.json({
-            success: false,
-            message: "Create user failed : Address minimum 10 characters",
-            result: null
-        })
-        return
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : Address minimum 10 characters"
+        )
     }
 
     if (email === undefined || !email.includes("@")) {
-        response.json({
-            success: false,
-            message: "Create user failed : Invalid email",
-            result: null
-        })
-        return
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : Invalid email"
+        )
     }
 
     if (password === undefined || password.length < 8) {
-        response.json({
-            success: false,
-            message: "Create user failed : Password too weak! minimum 8 characters",
-            result: null
-        })
-        return
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : Password too weak! minimum 8 characters"
+        )
     }
 
     if (confirm_password !== password) {
-        response.json({
-            success: false,
-            message: "Create user failed : Confirm password not match",
-            result: null
-        })
-        return
+        return httpResponse.badRequest(
+            response,
+            "Create user failed : Confirm password not match"
+        )
     }
 
     try {
-        const user = await userCredentialsModel.getUserCredentialsByEmail(email);
+        const user = await userCredentialsModel.getUserCredentialsByEmail(email)
 
         if (user) {
-            return response.status(400).json({
-                success: false,
-                message: "Email already used!",
-                results: null
-            });
+            return httpResponse.conflict(
+                response,
+                "Email already used!"
+            )
         }
 
         const hashedPassword = await GenerateHash(password)
-        const registeredUser = await userModel.createUsersWithProfileAndCredentials({ first_name, last_name, address }, { email, phone, password: hashedPassword })
+
+        const registeredUser =
+            await userModel.createUsersWithProfileAndCredentials(
+                { first_name, last_name, address },
+                { email, phone, password: hashedPassword }
+            )
+
         if (!registeredUser) {
-            throw new Error("Create user transaction fail!");
+            return httpResponse.serverError(
+                response,
+                "Create user transaction fail!"
+            )
         }
 
-        response.json({
-            success: true,
-            message: "Create user success !",
-            result: registeredUser
-        })
+        return httpResponse.created(
+            response,
+            "Create user success!",
+            registeredUser
+        )
+
     } catch (error) {
-        response.json({
-            success: true,
-            message: "Create users fail !" + error,
-            result: null
-        })
+        return httpResponse.serverError(
+            response,
+            "Create users fail! " + error
+        )
     }
 }
 
@@ -126,18 +137,17 @@ export async function createUsers(request, response) {
 export async function deleteUser(request, response) {
     try {
         const user = await userModel.deleteUser(request.params.id)
-        response.json({
-            success: true,
-            message: "Delete users success !",
-            result: user
-        })
 
+        return httpResponse.ok(
+            response,
+            "Delete user success!",
+            user
+        )
     } catch (error) {
-        response.json({
-            success: true,
-            message: "Delete users fail !" + error,
-            result: null
-        })
+        return httpResponse.serverError(
+            response,
+            "Delete user fail! " + error
+        )
     }
 }
 
@@ -149,17 +159,16 @@ export async function deleteUser(request, response) {
 export async function updateUser(request, response) {
     try {
         const user = await userModel.updateUser(request.params.id, request.body)
-        response.json({
-            success: true,
-            message: "Update users success !",
-            result: user
-        })
 
+        return httpResponse.ok(
+            response,
+            "Update user success!",
+            user
+        )
     } catch (error) {
-        response.json({
-            success: true,
-            message: "Update users fail !" + error,
-            result: null
-        })
+        return httpResponse.serverError(
+            response,
+            "Update user fail! " + error
+        )
     }
 }
