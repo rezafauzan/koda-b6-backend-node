d7699516088f94e47599cf338bc25eb260a30691
rezafauzan <rezafauzan593@gmail.com>
2026-04-15 18:28:51 +0700

    Fix: missing import on userProfile router

--- src/routes/user_profiles.router.js
+++ src/routes/user_profiles.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import AuthMiddleware from "../middleware/auth.middleware.js";
 import * as userProfileController from "../controllers/user_profiles.controller.js"
+import { uploadMiddleware } from "../middleware/upload.middleware.js";
 
 const userProfileRouter = Router()
 
